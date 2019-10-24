f38acaa8980bd607ea742a300b16f579aa14f311
Trung Nguyen <ntrung@pdx.edu>
2019-10-24 11:23:30 -0800

    Add logic to move bird up

--- script.js
+++ script.js
@@ -2,38 +2,46 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 var bird = new Image();
-bird.src ="images/bird.png";
+bird.src = "images/bird.png";
 
 var bg = new Image();
-bg.src ="images/bg.png";
+bg.src = "images/bg.png";
 
 var fg = new Image();
-fg.src ="images/fg.png";
+fg.src = "images/fg.png";
 
-var pipeNorth= new Image();
-pipeNorth.src ="images/pipeNorth.png";
+var TopPipe = new Image();
+TopPipe.src = "images/pipeNorth.png";
 
-var pipeSouth = new Image();
-pipeSouth.src ="images/pipeSouth.png";
+var BotPipe = new Image();
+BotPipe.src = "images/pipeSouth.png";
 
-var bird_x =10;
-var bird_y =150;
+var bird_x = 10;
+var bird_y = 150;
 
+function draw() {
+  ctx.drawImage(bg, 0, 0);
+  ctx.drawImage(TopPipe, 100, 0);
+  ctx.drawImage(BotPipe, 100, 0 + TopPipe.height + 80);
 
+  ctx.drawImage(fg, 0, canvas.height - fg.height);
 
-function draw()
-{
-    ctx.drawImage(bg,0,0);
-    ctx.drawImage(pipeNorth,100,0);
-    ctx.drawImage(pipeSouth,100,0+pipeNorth.height +75);
+  document.addEventListener(
+    "keyup",
+    function(event) {
+      if (event.keyCode == 38) {
+        console.log("bird_y", bird_y);
+        bird_y--;
+        console.log("after", bird_y);
+      }
+    },
+    true
+  );
 
-    ctx.drawImage(fg,0,canvas.height -fg.height);
+  bird_y++;
+  ctx.drawImage(bird, bird_x, bird_y);
 
-    ctx.drawImage(bird,bird_x,bird_y);
-
-    bird_y++;
-
-
-    requestAnimationFrame(draw); 
+  requestAnimationFrame(draw);
+  gravity = 1;
 }
 draw();
