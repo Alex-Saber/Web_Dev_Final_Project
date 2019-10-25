379bd2e022eb39232000b4b76671441fe366aed5
angelicphan <phan4@pdx.edu>
2019-10-25 16:29:25 -0700

    alex worked on jump and pipe

--- script.js
+++ script.js
@@ -17,57 +17,76 @@ var BotPipe = new Image();
 BotPipe.src = "images/pipeSouth.png";
 
 var GameOver = new Audio();
-GameOver.src = "sound/OHH.mp3"
+GameOver.src = "sound/OHH.mp3";
 var bird_x = 10;
 var bird_y = 150;
-var score =0;
+var score = 0;
 
-var pipe_array =[100000];
-pipe_array[0] = {x : canvas.width, y: 0};
-
+var jump = false;
+var jump_frame = false;
+var pipe_array = [100000];
+pipe_array[0] = { x: canvas.width, y: 0 };
 
 document.addEventListener("keyup", go_up);
 function go_up(event) {
   if (event.keyCode == 38) {
-    console.log("bird_y", bird_y);
-    bird_y -= 50;
-    console.log("after", bird_y);
+    if (jump === false) {
+      jump = true;
+    }
   }
 }
 
 function draw() {
   ctx.drawImage(bg, 0, 0);
   ctx.drawImage(fg, 0, canvas.height - fg.height);
+
   bird_y += 1;
+
+  if (jump && jump_frame <= 5) {
+    bird_y -= 15;
+    jump_frame += 1;
+  }
+  if (jump_frame > 5) {
+    jump_frame = 0;
+    jump = false;
+  }
+
   ctx.drawImage(bird, bird_x, bird_y);
 
-  for( var i=0; i<pipe_array.length;i++)
-  {
+  for (var i = 0; i < pipe_array.length; i++) {
+    // pipe_array[i].y += Math.random() * ()
     ctx.drawImage(TopPipe, pipe_array[i].x, pipe_array[i].y);
-    ctx.drawImage(BotPipe, pipe_array[i].x, pipe_array[i].y + TopPipe.height + 80);
+
+    ctx.drawImage(
+      BotPipe,
+      pipe_array[i].x,
+      pipe_array[i].y + TopPipe.height + 80
+    );
 
     pipe_array[i].x--;
 
-    if(pipe_array[i].x ==50 )
-    {
-      pipe_array.push({x : canvas.width, y: 0});
+    if (pipe_array[i].x == 50) {
+      pipe_array.push({ x: canvas.width, y: pipe_array[i].y + 10 });
     }
 
-    if(pipe_array[i].x ==0 )
-    {
+    if (pipe_array[i].x == 0) {
       score++;
     }
 
     //Collision logic
-      GameOver.play();
-    
 
+    console.log("bird");
+    console.log(bird_x);
+    console.log(bird_y);
+    console.log("pipe:");
+    console.log(pipe_array[i].x);
+    console.log(pipe_array[i].y);
+
+    GameOver.play();
   }
-  
-
 
   ctx.font = "30px Open Sans";
-  ctx.fillText("Score :" +score,10,canvas.height-20);
+  ctx.fillText("Score :" + score, 10, canvas.height - 20);
   requestAnimationFrame(draw);
 }
 draw();
