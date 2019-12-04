1f8d1696ed745e719fc434803022e2f6a5225f51
Trung Nguyen <ntrung@pdx.edu>
2019-12-03 16:14:05 -0800

    Fix some bugs with game

--- resources/bird.js
+++ resources/bird.js
@@ -1,137 +1,126 @@
 let bird = function() {
+  function playBird() {
+    var canvas = document.getElementById("flappy-canvas");
+    var ctx = canvas.getContext("2d");
 
-    document.addEventListener("keydown", move);
+    var pause = false;
+    var bird = new Image();
 
-    function move(e) {
-        if (e.keyCode == 13) {
-            //location.reload();
-            playBird();
+    var bg = new Image();
+    bg.src = "../images/bg.png";
+
+    var fg = new Image();
+    fg.src = "../images/fg.png";
+
+    var TopPipe = new Image();
+    TopPipe.src = "../images/pipeNorth.png";
+
+    var BotPipe = new Image();
+    BotPipe.src = "../images/pipeSouth.png";
+
+    //var GameOver = new Audio();
+    //GameOver.src = "../sound/OHH.mp3";
+    var bird_x = 10;
+    var bird_y = 150;
+    var score = 0;
+
+    var bird_score_update = false;
+
+    var jump = false;
+    var jump_frame = false;
+    var pipe_array = [100000];
+    pipe_array[0] = { x: canvas.width, y: 0 };
+
+    document.addEventListener("keyup", go_up);
+
+    function go_up(event) {
+      if (event.keyCode === 38) {
+        if (jump === false) {
+          jump = true;
         }
+      }
     }
 
-    function playBird() {
-        var canvas = document.getElementById("flappy-canvas");
-        var ctx = canvas.getContext("2d");
+    let gravity = 0.5;
 
-        var pause = false;
-        var bird = new Image();
+    function draw() {
+      ctx.drawImage(bg, 0, 0);
+      ctx.drawImage(fg, 0, canvas.height - fg.height);
 
-        var bg = new Image();
-        bg.src = "../images/bg.png";
+      if (pause == false) {
+        gravity += 0.1;
+        bird_y += gravity;
 
-        var fg = new Image();
-        fg.src = "../images/fg.png";
+        if (jump && jump_frame <= 5) {
+          gravity = 0;
+          bird_y -= 10;
+          jump_frame += 1;
+        }
+        if (jump_frame > 5) {
+          jump_frame = 0;
+          jump = false;
+        }
+      }
+      if (pause == false) {
+        bird.src = "../images/bird.png";
+      } else {
+        bird.src = "../images/ded_bird.png";
+      }
+      ctx.drawImage(bird, bird_x, bird_y);
 
-        var TopPipe = new Image();
-        TopPipe.src = "../images/pipeNorth.png";
+      for (var i = 0; i < pipe_array.length; i++) {
+        ctx.drawImage(TopPipe, pipe_array[i].x, pipe_array[i].y);
 
-        var BotPipe = new Image();
-        BotPipe.src = "../images/pipeSouth.png";
+        ctx.drawImage(
+          BotPipe,
+          pipe_array[i].x,
+          pipe_array[i].y + TopPipe.height + 5 * bird.height
+        );
 
-        //var GameOver = new Audio();
-        //GameOver.src = "../sound/OHH.mp3";
-        var bird_x = 10;
-        var bird_y = 150;
-        var score = 0;
-
-        var bird_score_update = false;
-
-        var jump = false;
-        var jump_frame = false;
-        var pipe_array = [100000];
-        pipe_array[0] = {x: canvas.width, y: 0};
-
-        document.addEventListener("keyup", go_up);
-
-        function go_up(event) {
-            if (event.keyCode === 38) {
-                if (jump === false) {
-                    jump = true;
-                }
-            }
+        if (pause == false) {
+          pipe_array[i].x--;
+        }
+        if (pipe_array[i].x === 50) {
+          pipe_array.push({
+            x: canvas.width,
+            y: Math.floor(Math.random() * TopPipe.height) - TopPipe.height
+          });
         }
 
-        document.addEventListener("keyup", restart);
-
-        function restart(event) {
-            if (event.keyCode === 13) {
-                bird_score_update = false;
-                return 1;
-                //window.location.reload(false);
-            }
+        if (pipe_array[i].x === 0) {
+          score++;
         }
 
-        let gravity = 0.5;
-
-        function draw() {
-            ctx.drawImage(bg, 0, 0);
-            ctx.drawImage(fg, 0, canvas.height - fg.height);
-
-            if (pause == false) {
-                gravity += 0.1;
-                bird_y += gravity;
-
-                if (jump && jump_frame <= 5) {
-                    gravity = 0;
-                    bird_y -= 10;
-                    jump_frame += 1;
-                }
-                if (jump_frame > 5) {
-                    jump_frame = 0;
-                    jump = false;
-                }
-            }
-            if (pause == false) {
-                bird.src = "../images/bird.png";
-            } else {
-                bird.src = "../images/ded_bird.png";
-            }
-            ctx.drawImage(bird, bird_x, bird_y);
-
-            for (var i = 0; i < pipe_array.length; i++) {
-                ctx.drawImage(TopPipe, pipe_array[i].x, pipe_array[i].y);
-
-                ctx.drawImage(
-                    BotPipe,
-                    pipe_array[i].x,
-                    pipe_array[i].y + TopPipe.height + 5 * bird.height
-                );
-
-                if (pause == false) {
-                    pipe_array[i].x--;
-                }
-                if (pipe_array[i].x === 50) {
-                    pipe_array.push({
-                        x: canvas.width,
-                        y: Math.floor(Math.random() * TopPipe.height) - TopPipe.height
-                    });
-                }
-
-                if (pipe_array[i].x === 0) {
-                    score++;
-                }
-
-                // //Collision logic
-                if (
-                    bird_x + bird.width >= pipe_array[i].x &&
-                    bird_x <= pipe_array[i].x + TopPipe.width &&
-                    (bird_y < pipe_array[i].y + TopPipe.height ||
-                        bird_y + bird.height >
-                        pipe_array[i].y + TopPipe.height + 5 * bird.height)
-                ) {
-                    // Pause the game
-                    pause = true;
-                    ctx.font = "bold 40px Arial";
-                    ctx.fillText("Game Over!", canvas.width / 2 - 100, canvas.height / 2);
-                    ctx.fillText("Score: ", canvas.width / 2 - 100, canvas.height / 2 + 40);
-                    ctx.fillText(score, canvas.width / 2 + 50, canvas.height / 2 + 40);
-                    ctx.font = "bold 20px Arial";
-                    ctx.fillText(
-                        "Press enter to try again",
-                        canvas.width / 2 - 100,
-                        canvas.height / 2 + 70
-                    );
-                    /*if (bird_score_update == false) {
+        // //Collision logic
+        if (
+          bird_x + bird.width >= pipe_array[i].x &&
+          bird_x <= pipe_array[i].x + TopPipe.width &&
+          (bird_y < pipe_array[i].y + TopPipe.height ||
+            bird_y + bird.height >
+              pipe_array[i].y + TopPipe.height + 5 * bird.height)
+        ) {
+          // Pause the game
+          pause = true;
+          ctx.font = "bold 40px Arial";
+          ctx.fillText("Game Over!", canvas.width / 2 - 100, canvas.height / 2);
+          ctx.fillText(
+            "Score: ",
+            canvas.width / 2 - 100,
+            canvas.height / 2 + 40
+          );
+          ctx.fillText(score, canvas.width / 2 + 50, canvas.height / 2 + 40);
+          ctx.font = "bold 20px Arial";
+          ctx.fillText(
+            "Press the Start button ",
+            canvas.width / 2 - 100,
+            canvas.height / 2 + 70
+          );
+          ctx.fillText(
+            "try again",
+            canvas.width / 2 - 100,
+            canvas.height / 2 + 90
+          );
+          /*if (bird_score_update == false) {
                       let url = "http://localhost:3000/user/update/score";
                       let request_body = {
                         name: "Bird game",
@@ -152,37 +141,36 @@ let bird = function() {
                       });
                       bird_score_update = true;
                     }*/
-                    let time = new Date();
-                    let activity = {
-                        timestamp: time,
-                        game: "Flappy Bird",
-                        score: score
-                        // username: USERNAME
-                    };
-                    if (bird_score_update === false) {
-                        updateUserActivityAndScores(activity);
-                        bird_score_update = true;
-                    }
-                }
-                if (pause == true) {
-                    bird_y += gravity;
-                    gravity += 0.1;
-                    if (bird_y >= canvas.height) {
-                        bird_y = 600;
-                    }
-                }
-
-                // GameOver.play();
-            }
-
-            // ctx.font = "30px Open Sans";
-            //ctx.fillText("Score :" + score, 10, canvas.height - 20);
-            requestAnimationFrame(draw);
+          let time = new Date();
+          let activity = {
+            Timestamp: time,
+            Game: "Flappy Bird",
+            Score: score
+            // username: USERNAME
+          };
+          if (bird_score_update === false) {
+            updateUserActivityAndScores(activity);
+            bird_score_update = true;
+          }
+        }
+        if (pause == true) {
+          bird_y += gravity;
+          gravity += 0.1;
+          if (bird_y >= canvas.height) {
+            bird_y = 600;
+          }
         }
 
-        draw();
+        // GameOver.play();
+      }
+
+      // ctx.font = "30px Open Sans";
+      //ctx.fillText("Score :" + score, 10, canvas.height - 20);
+      requestAnimationFrame(draw);
     }
 
-    playBird();
+    draw();
+  }
 
+  playBird();
 };
--- resources/shooter.js
+++ resources/shooter.js
@@ -1,206 +1,186 @@
-
 let shooter = function() {
+  let playShooter = function() {
+    var canvas = document.getElementById("invaders-canvas");
+    var ctx = canvas.getContext("2d");
 
-    document.addEventListener("keydown", move);
+    var ship = new Image();
+    var ship_x = canvas.width / 2;
+    var ship_y = canvas.height - 100;
+    ship.src = "../images/ship.gif";
 
-    function move(e) {
-        if (e.keyCode == 13) {
-            //location.reload();
-            playShooter();
-        }
+    var space = new Image();
+    space.src = "../images/space.png";
+
+    var alive = true;
+    var score = 0;
+    var left = false;
+    var right = false;
+    var up = false;
+    var down = false;
+    var shooter_score_update = false;
+
+    var enemies_total = 10;
+    var enemies_array = [];
+    var enemy_x = 10;
+    var enemy_y = 0;
+    var enemy_speed = 3;
+    var enemy = new Image();
+    enemy.src = "../images/enemy1.png";
+
+    var laser_length = 1000000;
+    var laser_array = [];
+    var laser = new Image();
+    laser.src = "../images/bullet.png";
+
+    document.addEventListener("keydown", keyDown, false);
+    document.addEventListener("keyup", keyUp, false);
+
+    function keyDown(e) {
+      if (e.keyCode == 39) right = true;
+      else if (e.keyCode == 37) left = true;
+      if (e.keyCode == 38) up = true;
+      else if (e.keyCode == 40) down = true;
+      if (e.keyCode == 88 && laser_array.length <= laser_length)
+        laser_array.push([ship_x, ship_y, 0]);
     }
 
-    let playShooter = function () {
-        var canvas = document.getElementById("invaders-canvas");
-        var ctx = canvas.getContext("2d");
+    function keyUp(e) {
+      if (e.keyCode == 39) right = false;
+      else if (e.keyCode == 37) left = false;
+      if (e.keyCode == 38) up = false;
+      else if (e.keyCode == 40) down = false;
+    }
 
-        var ship = new Image();
-        var ship_x = canvas.width / 2;
-        var ship_y = canvas.height - 100;
-        ship.src = "../images/ship.gif";
+    //add enemies
+    for (var i = 0; i < enemies_total; i++) {
+      enemies_array.push([enemy_x, enemy_y]);
+      enemy_x += enemy.width + 30;
+      if (enemy_x >= canvas.width) {
+        enemy_x = 10;
+      }
+    }
 
-        var space = new Image();
-        space.src = "../images/space.png";
+    function drawship() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(space, 0, 0);
+      if (alive == true) {
+        if (right) ship_x += 5;
+        else if (left) ship_x -= 5;
+        if (up) ship_y -= 5;
+        else if (down) ship_y += 5;
 
-        var alive = true;
-        var score = 0;
-        var left = false;
-        var right = false;
-        var up = false;
-        var down = false;
-        var shooter_score_update = false;
+        if (ship_x <= 0) ship_x = 0;
 
-        var enemies_total = 10;
-        var enemies_array = [];
-        var enemy_x = 10;
-        var enemy_y = 0;
-        var enemy_speed = 3;
-        var enemy = new Image();
-        enemy.src = "../images/enemy1.png";
+        if (ship_x + ship.width >= canvas.width)
+          ship_x = canvas.width - ship.width;
 
-        var laser_length = 1000000;
-        var laser_array = [];
-        var laser = new Image();
-        laser.src = "../images/bullet.png";
+        if (ship_y <= 0) ship_y = 0;
 
-        document.addEventListener("keydown", keyDown, false);
-        document.addEventListener("keyup", keyUp, false);
+        if (ship_y + ship.height >= canvas.height)
+          ship_y = canvas.height - ship.height;
 
-        function keyDown(e) {
-            if (e.keyCode == 39) right = true;
-            else if (e.keyCode == 37) left = true;
-            if (e.keyCode == 38) up = true;
-            else if (e.keyCode == 40) down = true;
-            if (e.keyCode == 88 && laser_array.length <= laser_length)
-                laser_array.push([ship_x, ship_y, 0]);
-            if (e.keyCode == 13) {
-                // playShooter();
-                return 1;
+        //move enenmies and draw
+        for (var i = 0; i < enemies_array.length; ++i) {
+          if (enemies_array[i][1] < canvas.height) {
+            enemies_array[i][1] += 1;
+          } else if (enemies_array[i][1] >= canvas.height) {
+            enemies_array[i][1] = 0;
+          }
+          ctx.drawImage(enemy, enemies_array[i][0], enemies_array[i][1]);
+        }
+        if (laser_array.length != 0) {
+          for (var i = 0; i < laser_array.length; i++) {
+            ctx.drawImage(laser, laser_array[i][0], laser_array[i][1]);
+          }
+        }
+        for (var i = 0; i < laser_array.length; i++) {
+          laser_array[i][1] -= 10;
+          if (laser_array[i][1] <= 0) {
+            laser_array[i].splice(i, 1);
+          }
+        }
+        //Collision
+        var remove = false;
+        for (var i = 0; i < laser_array.length; i++) {
+          for (var j = 0; j < enemies_array.length; j++) {
+            if (
+              laser_array[i][1] <= enemies_array[j][1] + enemy.height &&
+              laser_array[i][0] >= enemies_array[j][0] &&
+              laser_array[i][0] <= enemies_array[j][0] + enemy.width
+            ) {
+              remove = true;
+              enemies_array.splice(j, 1);
+              score += 1;
+              enemies_array.push([
+                Math.random() * (canvas.width - 10) + 1,
+                enemy_y
+              ]);
+              enemies_array.push([
+                Math.random() * (canvas.width - 10) + 1,
+                enemy_y
+              ]);
             }
+          }
+          if (remove == true) {
+            laser_array.splice(i, 1);
+            remove = false;
+          }
         }
+        //ship collision
+        for (var i = 0; i < enemies_array.length; i++) {
+          if (
+            ship_x > enemies_array[i][0] &&
+            ship_x < enemies_array[i][0] + enemy.width &&
+            ship_y > enemies_array[i][1] &&
+            ship_y < enemies_array[i][1] + enemy.height
+          ) {
+            alive = false;
+          }
+          if (
+            ship_x + ship.width < enemies_array[i][0] + enemy.width &&
+            ship_x + ship.width > enemies_array[i][0] &&
+            ship_y > enemies_array[i][1] &&
+            ship_y < enemies_array[i][1] + enemy.height
+          ) {
+            alive = false;
+          }
+          if (
+            ship_y + ship.height > enemies_array[i][1] &&
+            ship_y + ship.height < enemies_array[i][1] + enemy.height &&
+            ship_x > enemies_array[i][0] &&
+            ship_x < enemies_array[i][0] + enemy.width
+          ) {
+            alive = false;
+          }
 
-        function keyUp(e) {
-            if (e.keyCode == 39) right = false;
-            else if (e.keyCode == 37) left = false;
-            if (e.keyCode == 38) up = false;
-            else if (e.keyCode == 40) down = false;
+          if (
+            ship_y + ship.height > enemies_array[i][1] &&
+            ship_y + ship.height < enemies_array[i][1] + enemy.heighth &&
+            ship_x + ship.width < enemies[i][0] + enemy.width &&
+            ship_x + ship.width > enemies_array[i][0]
+          ) {
+            alive = false;
+          }
         }
-
-        //add enemies
-        for (var i = 0; i < enemies_total; i++) {
-            enemies_array.push([enemy_x, enemy_y]);
-            enemy_x += enemy.width + 30;
-            if (enemy_x >= canvas.width) {
-                enemy_x = 10;
-            }
-        }
-
-        function drawship() {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(space, 0, 0);
-            if (alive == true) {
-                if (right) ship_x += 5;
-                else if (left) ship_x -= 5;
-                if (up) ship_y -= 5;
-                else if (down) ship_y += 5;
-
-                if (ship_x <= 0) ship_x = 0;
-
-                if (ship_x + ship.width >= canvas.width)
-                    ship_x = canvas.width - ship.width;
-
-                if (ship_y <= 0) ship_y = 0;
-
-                if (ship_y + ship.height >= canvas.height)
-                    ship_y = canvas.height - ship.height;
-
-                //move enenmies and draw
-                for (var i = 0; i < enemies_array.length; ++i) {
-                    if (enemies_array[i][1] < canvas.height) {
-                        enemies_array[i][1] += 1;
-                    } else if (enemies_array[i][1] >= canvas.height) {
-                        enemies_array[i][1] = 0;
-                    }
-                    ctx.drawImage(enemy, enemies_array[i][0], enemies_array[i][1]);
-                }
-                if (laser_array.length != 0) {
-                    for (var i = 0; i < laser_array.length; i++) {
-                        ctx.drawImage(laser, laser_array[i][0], laser_array[i][1]);
-                    }
-                }
-                for (var i = 0; i < laser_array.length; i++) {
-                    laser_array[i][1] -= 10;
-                    if (laser_array[i][1] <= 0) {
-                        laser_array[i].splice(i, 1);
-                    }
-                }
-                //Collision
-                var remove = false;
-                for (var i = 0; i < laser_array.length; i++) {
-                    for (var j = 0; j < enemies_array.length; j++) {
-                        if (
-                            laser_array[i][1] <= enemies_array[j][1] + enemy.height &&
-                            laser_array[i][0] >= enemies_array[j][0] &&
-                            laser_array[i][0] <= enemies_array[j][0] + enemy.width
-                        ) {
-                            remove = true;
-                            enemies_array.splice(j, 1);
-                            score += 1;
-                            enemies_array.push([
-                                Math.random() * (canvas.width - 10) + 1,
-                                enemy_y
-                            ]);
-                            enemies_array.push([
-                                Math.random() * (canvas.width - 10) + 1,
-                                enemy_y
-                            ]);
-                        }
-                    }
-                    if (remove == true) {
-                        laser_array.splice(i, 1);
-                        remove = false;
-                    }
-                }
-                //ship collision
-                for (var i = 0; i < enemies_array.length; i++) {
-                    if (
-                        ship_x > enemies_array[i][0] &&
-                        ship_x < enemies_array[i][0] + enemy.width &&
-                        ship_y > enemies_array[i][1] &&
-                        ship_y < enemies_array[i][1] + enemy.height
-                    ) {
-                        alive = false;
-                        console.log(alive);
-                    }
-                    if (
-                        ship_x + ship.width < enemies_array[i][0] + enemy.width &&
-                        ship_x + ship.width > enemies_array[i][0] &&
-                        ship_y > enemies_array[i][1] &&
-                        ship_y < enemies_array[i][1] + enemy.height
-                    ) {
-                        alive = false;
-                        console.log(alive);
-                    }
-                    if (
-                        ship_y + ship.height > enemies_array[i][1] &&
-                        ship_y + ship.height < enemies_array[i][1] + enemy.height &&
-                        ship_x > enemies_array[i][0] &&
-                        ship_x < enemies_array[i][0] + enemy.width
-                    ) {
-                        alive = false;
-                        console.log(alive);
-                    }
-
-                    if (
-                        ship_y + ship.height > enemies_array[i][1] &&
-                        ship_y + ship.height < enemies_array[i][1] + enemy.heighth &&
-                        ship_x + ship.width < enemies[i][0] + enemy.width &&
-                        ship_x + ship.width > enemies_array[i][0]
-                    ) {
-                        alive = false;
-                        date = new Date();
-                        console.log(date);
-                    }
-                }
-                ctx.drawImage(ship, ship_x, ship_y);
-                requestAnimationFrame(drawship);
-                ctx.font = "bold 18px Arial";
-                ctx.fillStyle = "#fff";
-                ctx.fillText("Score: ", 490, 30);
-                ctx.fillText(score, 550, 30);
-            } //alive == false
-            else {
-                ctx.font = "bold 40px Arial";
-                ctx.fillText("Game Over!", canvas.width / 2 - 100, canvas.height / 2);
-                ctx.fillText("Score: ", canvas.width / 2 - 100, canvas.height / 2 + 40);
-                ctx.fillText(score, canvas.width / 2 + 50, canvas.height / 2 + 40);
-                ctx.font = "bold 20px Arial";
-                ctx.fillText(
-                    "Press enter to try again",
-                    canvas.width / 2 - 100,
-                    canvas.height / 2 + 70
-                );
-                /* if (shooter_score_update == false) {
+        ctx.drawImage(ship, ship_x, ship_y);
+        requestAnimationFrame(drawship);
+        ctx.font = "bold 18px Arial";
+        ctx.fillStyle = "#fff";
+        ctx.fillText("Score: ", 490, 30);
+        ctx.fillText(score, 550, 30);
+      } //alive == false
+      else {
+        ctx.font = "bold 40px Arial";
+        ctx.fillText("Game Over!", canvas.width / 2 - 100, canvas.height / 2);
+        ctx.fillText("Score: ", canvas.width / 2 - 100, canvas.height / 2 + 40);
+        ctx.fillText(score, canvas.width / 2 + 50, canvas.height / 2 + 40);
+        ctx.font = "bold 20px Arial";
+        ctx.fillText(
+          "Press the Start button to try again",
+          canvas.width / 2 - 100,
+          canvas.height / 2 + 70
+        );
+        /* if (shooter_score_update == false) {
                   let url = "http://localhost:3000/user/update/score";
                   let request_body = {
                     name: "Space invader game",
@@ -221,19 +201,21 @@ let shooter = function() {
                   });
                   shooter_score_update = true;
                 }*/
-                let time = new Date();
-                let activity = {
-                    timestamp: time,
-                    game: "Space Invaders",
-                    score: score
-                    //username: USERNAME
-                };
-                updateUserActivityAndScores(activity);
-            }
+        if (shooter_score_update == false) {
+          let time = new Date();
+          let activity = {
+            Timestamp: time,
+            Game: "Space Invaders",
+            Score: score
+            //username: USERNAME
+          };
+          updateUserActivityAndScores(activity);
+          shooter_score_update = true;
         }
+      }
+    }
 
-        drawship();
-    };
-    playShooter();
-
+    drawship();
+  };
+  playShooter();
 };
