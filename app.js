0fedc2649c1e3733f538b6f6b14b018c6ee11f16
angelicphan <phan4@pdx.edu>
2019-12-05 13:01:47 -0800

    crash fix?

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const helmet = require("helmet");
 const fs = require("fs");
 var MongoClient = require("mongodb").MongoClient;
